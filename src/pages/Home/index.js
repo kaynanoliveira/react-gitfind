c00e54f79ff1060e54543074536702ada85c2a5c
kayna <kaynanoliveira357@gmail.com>
2025-04-23 19:53:20 -0300

    commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,22 @@
+import { Header } from "../../components/Header";
+import background from '../../assets/image.png'
+import './styles.css'
+
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Header />
+      <div className="conteudo">
+          <img src={background} className="background" alt="background app" />
+          <div className="info">
+            <div>
+              <input className="usuario" placeholder="@usuario"></input>
+              <button>Buscar</button>
+            </div>
+          </div>
+
+      </div>
+    </div>
   );
 }
 
